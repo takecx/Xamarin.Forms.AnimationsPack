06a835ae2ab23129071716087a79f7f07ada600b
noguchi takeshi <panda531531@gmail.com>
2018-10-12 14:37:43 +0900

    add AnimationDemo app

---
+++ demo/XFAnimationDemo/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XFAnimationDemo
+{
+	public class MainPageViewModel : INotifyPropertyChanged
+	{
+		#region Property
+		protected bool _isAnimationWorking;
+		public bool IsAnimationWorking
+		{
+			get { return _isAnimationWorking; }
+			set
+			{
+				_isAnimationWorking = value;
+				OnPropertyChanged(nameof(IsAnimationWorking));
+			}
+		}
+		#endregion
+
+		#region Commands
+		public ICommand RunAnimation { get; private set; }
+		#endregion
+
+		public MainPageViewModel()
+		{
+			// Create commands
+			RunAnimation = new RunAnimationCommand(this);
+
+			// Initialize
+			IsAnimationWorking = false;
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
---
+++ demo/XFAnimationDemo/RunAnimationCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace XFAnimationDemo
+{
+	internal class RunAnimationCommand : ICommand
+	{
+		private MainPageViewModel mainPageViewModel;
+
+		public RunAnimationCommand(MainPageViewModel mainPageViewModel)
+		{
+			this.mainPageViewModel = mainPageViewModel;
+		}
+
+		public event EventHandler CanExecuteChanged;
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			Task.Run(() =>
+			{
+				mainPageViewModel.IsAnimationWorking = true;
+				Thread.Sleep(2000);
+			}).ContinueWith((r) =>
+			{
+				mainPageViewModel.IsAnimationWorking = false;
+			});
+		}
+	}
+}
